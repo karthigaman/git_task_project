d5bb7158ad41855532ba998c24f884837dca498c
karthigaman <karthiga.rj@gmail.com>
2024-03-15 11:22:39 +0000

    test

---
+++ helloWorld.py
@@ -0,0 +1,11 @@
+#Task one
+# Using an input value to get an output
+# Giving an input value for the variables
+ 
+name1=input("Please enter your name : ")
+
+age1=input("Please enter your age : ")
+
+# Prints a value as a string
+
+print("Git is awesome!")
